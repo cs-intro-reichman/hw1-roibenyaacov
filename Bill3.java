43c727f464838558fa9566c093aa10597ebfd082
roibenyaacov <roi.benyaacov@post.runi.ac.il>
2024-11-11 18:56:22 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 		String name3 = args[2];
 		int sum = Integer.parseInt(args[3]);
 		double pay = Math.ceil((double)sum/3);
-		System.out.println(" Dear " + name1 + "," + name2 + ",and " + name3 + ": pays " + pay + "Shekels each" );
+		System.out.println(" Dear " + name3 + "," + name2 + ",and " + name1 + ": pays " + pay + " Shekels each" );
 	}
 }
